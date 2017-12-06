2ec165e6183552e645b1f69b72fa86f03739594c
bobbyquennell <bobbyquennell@gmail.com>
2017-12-06 17:30:40 +1100

    update header styling

--- docs/app/components/home/Home.js
+++ docs/app/components/home/Home.js
@@ -5,14 +5,15 @@ import style from './style.scss';
 const Home = function() {
   const clickHandler =()=>{console.log("haha");};
   return (
-    <div>
-      <head className={style.head}>
+    <div className={style.root}>
+      <div className={style.head}>
+        <img className={style.icon} src="../../../../favicon.ico" alt=""/>
         <nav className={style.nav}>
           <Link className={style.navitem} to="/">Docs</Link>
           <Link className={style.navitem} to="/">Blog</Link>
-          <Link className={style.navitem} to="/">Github</Link>
+          <a className={style.navitem} href="https://github.com/bobbyquennell/react-arsenal">Github</a>
         </nav>
-      </head>
+      </div>
       <h2>this is not a home page</h2>
       <Link to="/about">About</Link>
       <br/>
