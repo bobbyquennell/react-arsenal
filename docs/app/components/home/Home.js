537201eb5eb009ffd516da3a36fed9f6467fe89c
bobbyquennell <bobbyquennell@gmail.com>
2017-12-11 17:08:16 +1100

    update home page

--- docs/app/components/home/Home.js
+++ docs/app/components/home/Home.js
@@ -6,18 +6,31 @@ const Home = function() {
   const clickHandler =()=>{console.log("haha");};
   return (
     <div className={style.root}>
-      <div className={style.head}>
-        <img className={style.icon} src="../../../../favicon.ico" alt=""/>
-        <nav className={style.nav}>
-          <Link className={style.navitem} to="/">Docs</Link>
-          <Link className={style.navitem} to="/">Blog</Link>
-          <a className={style.navitem} href="https://github.com/bobbyquennell/react-arsenal">Github</a>
-        </nav>
+      <div className={style.headblock}>
+        <div className={style.head}>
+          <img className={style.icon} src="../../../../favicon.ico" alt=""/>
+          <nav className={style.nav}>
+            <Link className={style.navitem} to="/">Docs</Link>
+            <Link className={style.navitem} to="/">Blog</Link>
+            <a className={style.navitem} href="https://github.com/bobbyquennell/react-arsenal">Github</a>
+          </nav>
+          <div className={style.burger}>
+            <div className={style.burgerbar}/>
+            <div className={style.burgerbar}/>
+            <div className={style.burgerbar}/>
+          </div>
+        </div>
       </div>
-      <h2>this is not a home page</h2>
-      <Link to="/about">About</Link>
-      <br/>
-      <br/>
+      <div className={style.maincontent} >
+        <div className={style.hero}>
+          <h2>React Arsenal</h2>
+          <p>React Arsenal is a set of React components that implements Google Material Design specification. It's built on top of some the trendiest proposals like CSS Modules (written in SASS), Webpack and ES6. The library harmoniously integrates with your Webpack workflow and it's easily customizable and very flexible.
+Check our awesome documentation (which is built using React Toolbox) and try all the components with live examples. We've created a playground so you don't need to install anything to fall in love with our components, so fun!</p>
+          <Link to="/about">About</Link>
+          <br/>
+          <br/>
+        </div>
+      </div>
     </div>
   );
 };
