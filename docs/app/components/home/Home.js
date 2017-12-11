81ff4207e5d448f3a06cc3ca9dc221f56ad17100
Bobby Quennell <bobbyquennell@gmail.com>
2017-12-11 23:20:43 +1100

    update

--- docs/app/components/home/Home.js
+++ docs/app/components/home/Home.js
@@ -26,9 +26,13 @@ const Home = function() {
           <h2>React Arsenal</h2>
           <p>React Arsenal is a set of React components that implements Google Material Design specification. It's built on top of some the trendiest proposals like CSS Modules (written in SASS), Webpack and ES6. The library harmoniously integrates with your Webpack workflow and it's easily customizable and very flexible.
 Check our awesome documentation (which is built using React Toolbox) and try all the components with live examples. We've created a playground so you don't need to install anything to fall in love with our components, so fun!</p>
-          <Link to="/about">About</Link>
           <br/>
           <br/>
+        </div>
+      </div>
+      <div className={style.footerblock}>
+        <div className={style.footer}>
+          <p>React Arsenal@2017</p>
         </div>
       </div>
     </div>
