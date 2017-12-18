159eb887369a6d79889d22dec7e722775d528515
bobbyquennell <bobbyquennell@gmail.com>
2017-12-19 09:29:07 +1100

    fix setState typo

--- docs/app/components/docs/democontainer/democontainer.js
+++ docs/app/components/docs/democontainer/democontainer.js
@@ -9,20 +9,17 @@ class DemoContainer extends React.Component {
     };
   }
 
-  toggleCode = event =>{
+  toggleCode = (event)=>{
     event.preventDefault();
-    this.setSate(prevSate =>{
-      return { showCode: !prevSate.showCode};
-    });
+    this.setState(
+      prevState=>{ return { showCode: !prevState.showCode}; }
+    );
   }
 
   render() {
     const { showCode } = this.state;
     const { code, description, name} = this.props.demo;
 
-    //Must use CommonJS require to dynamically require because ES6 Modules must be
-    //statically analyzable.
-    console.log(`../../../../examples/${this.props.componentName}/${name}`);
     const DemoComponent = require(`../../../../examples/${this.props.componentName}/${name}`).default;
     return (
       <div className="example">
