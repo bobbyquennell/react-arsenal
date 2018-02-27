84b3bfdfb2d19840a6c19be922352e9f44e7b073
Bobby Quennell <bobbyquennell@gmail.com>
2018-02-27 22:34:15 +1100

    update styling of Grid Example

--- docs/examples/Grid/ExampleGrid.js
+++ docs/examples/Grid/ExampleGrid.js
@@ -3,7 +3,7 @@ import Grid from 'react-arsenal/Grid';
 import style from './style.scss';
 const ExampleGrid = function(){
   return (
-    <Grid className={style.root}>
+    <Grid className={style.root} justify="center">
       <Grid item><p>Lorem ipsum</p></Grid>
       <Grid item><p>Lorem ipsum</p></Grid>
     </Grid>
